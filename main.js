3be4447ec7a7044ac579ff0c0e4339867d03a7b8
andrew-zachary <andrew_zachary@outlook.com>
2023-05-26 00:00:00 +0000

    setup alpine-data-navmenu

--- main.js
+++ main.js
@@ -2,6 +2,13 @@ import './style.scss';
 
 import Alpine from 'alpinejs';
 import collapse from '@alpinejs/collapse';
+
+Alpine.data('navmenu', () => ({
+  open: false,
+  toggle() {
+    this.open = !this.open;
+  }
+}));
  
 Alpine.plugin(collapse);
 
