b4ae6e57ebb1d8ec40bd8a76a92fb01a612d999a
andrew-zachary <andrew_zachary@outlook.com>
2023-05-27 00:00:00 +0000

    remove unnecessary classes

--- main.js
+++ main.js
@@ -1,14 +1,7 @@
-import './style.scss';
+import './style.css';
 
 import Alpine from 'alpinejs';
 import collapse from '@alpinejs/collapse';
-
-Alpine.data('navmenu', () => ({
-  open: false,
-  toggle() {
-    this.open = !this.open;
-  }
-}));
  
 Alpine.plugin(collapse);
 
